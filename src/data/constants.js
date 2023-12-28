ef79d8a5b3714b040958e4898398e660d8129f79
Naveen-131 <naveenmadhavan131@gmail.com>
2023-12-28 12:51:11 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,302 @@
+export const Bio = {
+  name: "Naveen Kumar",
+  roles: [    
+    "Java Full Stack Developer",
+    "Backend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Naveen-131",
+  resume:
+    "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/docs/NaveenKumar+SDE.pdf",
+  linkedin: "https://www.linkedin.com/in/naveenkumar131/",
+  instagram: "https://www.instagram.com/n.a.v.e.e.n_sai/",
+  email: "naveenmadhavan131@gmail.com",
+  twitter:"https://twitter.com/Naveen_sai13",
+};
+
+export const skills = [
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      },
+      {
+        name: "Spring",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "Spring Boot",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "Spring MVC",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "JPA",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      }, 
+      {
+        name: "Python",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Python.png",
+      },
+      {
+        name: "Oracle SQL",
+        image: "https://www.vectorlogo.zone/logos/oracle/oracle-ar21.svg",
+      },
+      {
+        name: "PostgreSQL",
+        image:
+          "https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg",
+      },
+      {
+        name: "MySQL",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },   
+      {
+        name: "MongoDB",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }    
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Finacle CRM",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Finacle.png",
+      },
+      {
+        name: "Finacle Customization",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Finacle_2.png",
+      },
+      {
+        name: "Rest API",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/restAPI.png",
+      },
+      {
+        name: "Microservices",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/microservices.png",
+      },
+
+      {
+        name: "TDD",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/TDD.png",
+      },
+      {
+        name: "Unit Testing",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Unit+Testing.png",
+      },
+      {
+        name: "UAT",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/UAT.png",
+      },
+      {
+        name: "XML",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/xml.png",
+      },
+      {
+        name: "JSON",
+        image:
+        "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/json.png",
+      },
+      {
+        name: "Agile Development",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Agile.png",
+      },
+
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "JIRA",
+        image:
+          "https://www.vectorlogo.zone/logos/atlassian_jira/atlassian_jira-icon.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Swagger",
+        image:
+          "https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/swagger.svg",
+      },
+      {
+        name: "Eclipse",
+        image:
+          "https://www.vectorlogo.zone/logos/eclipse/eclipse-icon.svg",
+      },
+      {
+        name: "IntelliJ",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/IntelliJ.png",
+      },
+      {
+        name: "Oracle SQL Developer",
+        image:
+          "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/SQL_developer.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.logo.wine/a/logo/Capgemini/Capgemini-Logo.wine.svg",
+    role: "Senior Software Engineer ",
+    company: "Capgemini.",
+    date: "Jan 2023 - Present",
+    desc: "I work on the Finacle Banking Product as a backend developer, contributing by creating new functionalities and features. I am proficient in delivering code bug fixes for existing system issues based on business client requirements. Additionally, I have successfully resolved over 6500 account data issues in the production environment",
+    skills: [
+      "Java",
+      "Oracle SQL",
+      "XML",
+      "SOAP",
+      "Finacle CRM",
+      "Unit Testing",
+      "UAT",
+      "Microservices",
+
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://www.logo.wine/a/logo/Capgemini/Capgemini-Logo.wine.svg",
+    role: "Software Engineer",
+    company: "Capgemini",
+    date: "July 2021 - Dec 2022",
+    desc: "Worked on Finacle Banking Product, as a Java Full Stack Developer, responsible for creating new functionalities and features and proficient in delivering code bug fixes for existing system issues based on business client requirements.",
+    skills: [
+      "Java",
+      "Oracle SQL",
+      "Spring Boot",
+      "Spring MVC",
+      "Finacle CRM",
+      "Rest APIs",
+      "Microservices",
+      "JSON",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJs",
+    ],
+   
+  },
+  {
+    id: 1,
+    img: "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Crescent_Technosoft.png",
+    role: "Web developer Internship",
+    company: "Crescent Technosoft",
+    date: "May 2019 - July 2019",
+    desc: "Acquired knowledge and experience in PHP, HTML, CSS, and Bootstrap, enhancing technical versatility.Collaborated with the team to design and implement login and user modules, enhancing the E-Commerce system's functionality.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "PHP",
+      "Bootstrap",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://findlogovector.com/wp-content/uploads/2022/05/vellore-institute-of-technology-vit-logo-vector-2022.png",
+    school: "VIT University , Chennai",
+    date: "July 2016 - Jun 2021",
+    grade: "7.82 CGPA",
+    desc: "I completed M.Tech Software Engineering (5 Year Integrated) at VIT University,Chennai. I have taken courses in Data Structures, Algorithms, Object Oriented Programming, Software Architecture and Design, Database Management System (DBMS), Web Technologies, Problem Solving, Project Management, Agile Development, Software Design and Development,Operating Systems, and Computer Networks, among others.",
+    degree: "Master of Technology in Software Engineering (Integrated)",
+  },
+  {
+    id: 1,
+    img: "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/Vetri_Vikaas.png",
+    school: "Vetri Vikaas School, Tamilnadu",
+    date: "Apr 2015 - Apr 2016",
+    grade: "84.3%",
+    desc: "I completed my class 12 high school education at Vetri Vikaas Higher Secondary School, Namakkal, where I studied Science with Computer Science.",
+    degree: "HSC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://naveen-portfolio.s3.ap-south-1.amazonaws.com/Logo/SFS.png",
+    school: "St.Francis Desales Hr Sec School, Tamilnadu",
+    date: "Apr 2013 - Apr 2014",
+    grade: "90.2%",
+    desc: "I completed my class 10 education at St.Francis Desales Matric Higher Secondary School, Vellore",
+    degree: "SSLC(X)",
+  },
+];
+
